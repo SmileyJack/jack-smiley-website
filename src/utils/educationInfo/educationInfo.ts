af2b8c8f20b9364031f44a1691f53cc288c491af
SmileyJack <jacksmiley26@gmail.com>
2025-04-11 04:35:26 -0500

    slight edit

--- src/utils/educationInfo/educationInfo.ts
+++ src/utils/educationInfo/educationInfo.ts
@@ -4,12 +4,14 @@ export const educationInfo = [
         school: "University of Nebraska-Lincoln",
         program: "Jeffery S. Raikes School",
         gpa: "3.7",
-        description: ""
+        description: "Highly competitive "
         
     },
 
     {
         school: "Elkhorn South High School",
+        gpa: "4.3",
+        description: ""
         
     }
 ]
