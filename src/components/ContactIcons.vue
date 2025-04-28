84169c0e8b0ca1db7b5533f1581ddea5a6b2bb46
SmileyJack <jacksmiley26@gmail.com>
2025-04-28 01:41:39 -0500

    bingues

--- src/components/ContactIcons.vue
+++ src/components/ContactIcons.vue
@@ -1,15 +1,15 @@
 <template>
    <div :class="['flex', 'justify-start', screen === 'phone' ? 'mt-2' : 'wordPadder']">
-      <a href="your_resume_link_here" class="contactBox" target="_blank" @click="logContactIconClick('resume')">
+      <a href="your_resume_link_here" class="contactBox" target="_blank">
         <img src="icons/resume.png" alt="Resume" />
       </a>
-      <a href="https://www.linkedin.com/in/jack-b-smiley/" class="contactBox" target="_blank" @click="logContactIconClick('linkedin')">
+      <a href="https://www.linkedin.com/in/jack-b-smiley/" class="contactBox" target="_blank">
         <img src="icons/linkedin.png" alt="LinkedIn" />
       </a>
-      <a href="https://github.com/SmileyJack" class="contactBox" target="_blank" @click="logContactIconClick('github')">
+      <a href="https://github.com/SmileyJack" class="contactBox" target="_blank">
         <img src="icons/github.png" alt="GitHub" />
       </a>
-      <a href="mailto:jackbsmiley@gmail.com?subject=&body=" class="contactBox" target="_blank" @click="logContactIconClick('email')">
+      <a href="mailto:jackbsmiley@gmail.com?subject=&body=" class="contactBox" target="_blank">
         <img src="icons/email.png" alt="Email" />
       </a>
     </div>
